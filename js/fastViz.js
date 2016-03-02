6669c2f66f3ccaed0cf70bbdfcf2ad9c60432753
Tony Glover <tony.glover@nottingham.ac.uk>
2016-03-02 16:13:48 +0000

    Oauth signature additions

--- js/fastViz.js
+++ js/fastViz.js
@@ -189,16 +189,34 @@ FastApp.prototype.parseData = function() {
 
 FastApp.prototype.getAudioData = function() {
     //Get audio preview
+    var now = Math.round(new Date().getTime() / 1000);
+    var httpMethod = 'GET',
+        url = 'http://previews.7digital.com/clip/8514023',
+        parameters = {
+            oauth_consumer_key : '7dqw7pfc7sbw',
+            oauth_nonce : '343385748',
+            oauth_timestamp : now,
+            oauth_signature_method : 'HMAC-SHA1',
+            oauth_version : '1.0'
+        },
+        consumerSecret = 'qqx9pe6s6rfhnv37',
+        tokenSecret = '',
+    // generates a RFC 3986 encoded, BASE64 encoded HMAC-SHA1 hash
+        encodedSignature = oauthSignature.generate(httpMethod, url, parameters, consumerSecret, tokenSecret),
+    // generates a BASE64 encode HMAC-SHA1 hash
+        signature = oauthSignature.generate(httpMethod, url, parameters, consumerSecret, tokenSecret,
+            { encodeSignature: false});
+
     var xhr = new XMLHttpRequest();
     var url = "http://previews.7digital.com/clip/";
-    var trackID = 2315490;
+    var trackID = 8514023;
     var key = "?oauth_consumer_key=7dqw7pfc7sbw";
     var country = "&country=GB";
-    var oauthNonce = "&oauth_nonce=617563411";
+    var oauthNonce = "&oauth_nonce=343385748";
     var sigMethod = "&oauth_signature_method=HMAC-SHA1";
-    var oauthTimestamp = "&oauth_timestamp=1456876956";
+    var oauthTimestamp = "&oauth_timestamp=" + now;
     var oauthVersion = "&oauth_version=1.0";
-    var oauthSig = "&oauth_signature=uATMVLzbxV8PD11DgJkWkz0u3II%3D";
+    var oauthSig = "&oauth_signature=" + encodedSignature;
 
     var previewURL = url + trackID + key + country + oauthNonce + sigMethod + oauthTimestamp + oauthVersion + oauthSig;
     xhr.open("GET", previewURL, true);
